6944606239784457086436a62e880a353558a5da
Byoungseon Jeon <bjeon11@gmail.com>
2019-05-29 13:35:31 -0400

    Update python_datacamp.py

--- python_datacamp.py
+++ python_datacamp.py
@@ -22,3 +22,21 @@ fruits = ["apple", "banana", "orange"]
 iter_fruits = iter(fruits)
 print(next(iter_fruits))
 print(next(iter_fruits))
+
+manynumber = iter(range(10**100)) # Doesn't store statically. Saves memory
+
+# Reading a large file chunk by chunk (saves memory)
+for chunk in pd.read_csv('my.csv',chunksize=10):
+  for word in chunk['item']:
+    # do somehting here
+    
+# List comprehension
+a_list = [i for i in range(10)]
+b_list = [1 if x >5 else -1 for x in a_list]    
+
+# Generator
+## dynamic version of comprehension. Good for very large data or streaming
+## [] => ()
+a_list = (i for i in range(10))
+print(next(a_list))
+
